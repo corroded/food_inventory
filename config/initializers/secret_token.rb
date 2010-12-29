96d144c6d8a48e481e1218a17de29e3070d27f1b
Eumir Gaspar <eumir@aelogica.com>
2010-12-30 02:52:55 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodInventory::Application.config.secret_token = '196da2dcdc5510dc71697a3f297a0465e70a0d2e4080a2898bcfcbb0f762ebbbd35eac9a007aa9768a4b7bba7672c80a8f5bb9a907d7d152c7b884bd9879237f'
